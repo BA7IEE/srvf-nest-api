4e0db500965b5b8a91aff83bf3e48d4486d0b8f8
David 老灯 <tungwerl@gmail.com>
2026-05-21 17:56:23 +0800

    refactor: extract attendances contribution calculator

--- src/modules/attendances/attendances.module.ts
+++ src/modules/attendances/attendances.module.ts
@@ -9,6 +9,7 @@ import {
   AttendanceSheetsResourceController,
 } from './attendances.controller';
 import { AttendancesService } from './attendances.service';
+import { ContributionCalculator } from './contribution-calculator';
 import { AppMyAttendanceRecordsController } from './controllers/app-my-attendance-records.controller';
 
 // V2 批次 6 PR #6(D6 v1.1 §8 / 第二波最后一批):导入 AuditLogsModule 以注入 AuditLogsService,
@@ -32,6 +33,6 @@ import { AppMyAttendanceRecordsController } from './controllers/app-my-attendanc
     AttendanceRecordsMeController,
     AppMyAttendanceRecordsController,
   ],
-  providers: [AttendancesService, AppMyAttendanceRecordsService],
+  providers: [AttendancesService, AppMyAttendanceRecordsService, ContributionCalculator],
 })
 export class AttendancesModule {}
